f2895d3921951c44b3998f989f9b186fcef0a248
Steven Chong <7chong+ste80@gmail.com>
2018-12-05 00:10:00 +0800

    .

--- src/components/YoutubeComponent.tsx
+++ src/components/YoutubeComponent.tsx
@@ -1,4 +1,4 @@
-import { Fab } from '@material-ui/core';
+import { CircularProgress, Fab } from '@material-ui/core';
 import { createStyles, withStyles } from '@material-ui/core/styles';
 import { Theme } from '@material-ui/core/styles/createMuiTheme';
 import { CameraFront, CameraRear, PhotoCamera } from '@material-ui/icons';
@@ -20,6 +20,9 @@ const styles = ({ spacing }: Theme) =>
   createStyles({
     container: {
       position: 'relative'
+    },
+    progress: {
+      margin: spacing.unit * 2
     },
     screenshot: {
       margin: spacing.unit,
@@ -68,9 +71,10 @@ const screenshotHandler = async (addImagesFunc: typeof addImages) => {
 
 const YoutubeComponent = ({
   classes,
-  facingMode,
+  youtubeUrl,
+  mp4Url,
   showMessage,
-  switchFacingMode,
+  changeYoutubeUrl,
   addImages
 }) => (
   <div className={classes.container}>
@@ -82,26 +86,27 @@ const YoutubeComponent = ({
     >
       <PhotoCamera />
     </Fab>
-    <video
-      ref={videoRef}
-      width={WIDTH}
-      height={HEIGHT}
-      controls
-      autoPlay
-      loop
-      muted
-      style={{
-        borderWidth: 5,
-        borderStyle: 'solid',
-        borderColor: '#ccc'
-      }}
-      crossOrigin="anonymous"
-    >
-      <source
-        src="https:\/\/r4---sn-vgqskned.googlevideo.com\/videoplayback?clen=421372828&expire=1543956755&ipbits=0&pl=16&mime=video%2Fmp4&sparams=clen%2Cdur%2Cei%2Cgir%2Cid%2Cinitcwndbps%2Cip%2Cipbits%2Citag%2Clmt%2Cmime%2Cmm%2Cmn%2Cms%2Cmv%2Cpl%2Cratebypass%2Crequiressl%2Csource%2Cexpire&itag=18&ms=au%2Crdu&initcwndbps=2785000&mv=m&mt=1543935035&mn=sn-vgqskned%2Csn-vgqs7nl7&mm=31%2C29&ei=s5QGXObiI8XR8wSwrJWQDQ&id=o-AOxzKpLthxADq5E9xIehcz39akVv4-Yg9TdKTlK5W6Js&c=WEB&signature=18F79D17185ADF231BCD0C6C2C5C17680748FE94.94682E3D28D70C4E5DBD457600C73FF181481517&gir=yes&requiressl=yes&ip=54.87.68.138&fvip=2&key=yt6&lmt=1540224087351148&dur=6875.312&source=youtube&ratebypass=yes&txp=5531432"
-        type="video/mp4"
-      />
-    </video>
+    <input type="text" value={youtubeUrl} onChange={changeYoutubeUrl} />
+    {!mp4Url ? (
+      <CircularProgress className={classes.progress} />
+    ) : (
+      <video
+        ref={videoRef}
+        width={WIDTH}
+        height={HEIGHT}
+        controls
+        autoPlay
+        loop
+        muted
+        style={{
+          borderWidth: 5,
+          borderStyle: 'solid',
+          borderColor: '#ccc'
+        }}
+      >
+        <source src={mp4Url} type="video/mp4" />
+      </video>
+    )}
   </div>
 );
 
