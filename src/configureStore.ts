3ae119fcd594456d3c2adf864fd5b536d5bdc315
Steven Chong <7chong+ste80@gmail.com>
2018-12-06 17:15:33 +0800

    put json in public

--- src/configureStore.ts
+++ src/configureStore.ts
@@ -1,17 +1,18 @@
-import { rootEpic, rootReducer, RootState } from './reducers';
-import { applyMiddleware, createStore } from 'redux';
-import { combineEpics, createEpicMiddleware } from 'redux-observable';
-import { fetchMp4Url } from './actions';
-import { FACINGMODE_REAR, DEFAULT_YOUTUBE_URL } from './constants';
+import { rootEpic, rootReducer, RootState } from "./reducers";
+import { applyMiddleware, createStore } from "redux";
+import { combineEpics, createEpicMiddleware } from "redux-observable";
+import { fetchMp4Url } from "./actions";
+import { FACINGMODE_REAR, DEFAULT_YOUTUBE_URL } from "./constants";
+import { loadTinyFaceDetectorModel } from "face-api.js";
 
 const initState: RootState = {
   cameraPanel: {
-    tab: 'one',
-    message: '',
+    tab: "one",
+    message: "",
     images: [],
     facingMode: FACINGMODE_REAR,
     youtubeUrl: DEFAULT_YOUTUBE_URL,
-    mp4Url: ''
+    mp4Url: ""
   }
 };
 
@@ -24,5 +25,8 @@ export default () => {
   );
   epicMiddleware.run(rootEpic);
   store.dispatch(fetchMp4Url(DEFAULT_YOUTUBE_URL));
+  (async () => {
+    await loadTinyFaceDetectorModel("/");
+  })();
   return store;
 };
